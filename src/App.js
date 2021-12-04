c29f9fb919da0d5f91f2cdf80711e2ed5dc4ee7c
Andres Castañeda <af.castaneda223@gmail.com>
2021-12-04 12:43:40 -0500

    add react component to connect to API

--- src/App.js
+++ src/App.js
@@ -1,56 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getGreeting } from './redux/app/app';
 
 function App() {
+  const dispatch = useDispatch();
+  const fetchApi = 'http://localhost:3000/api/v1/greetings';
+
+  const myGreeting = useSelector((state) => state.greeting);
+
+  useEffect(() => {
+    const apiGreeting = async () => {
+      const fetchGreeting = await fetch(fetchApi);
+      const greeting = await fetchGreeting.json();
+      return dispatch(getGreeting(greeting));
+    };
+    if (myGreeting.length === 0) {
+      apiGreeting();
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+      <h1>Hello react front end</h1>
+      <h2>{myGreeting.greeting}</h2>
     </div>
   );
 }
